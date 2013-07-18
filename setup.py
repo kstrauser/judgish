69f1c5fa42238bef0cb5f223043d4ca85d60a333
Kirk Strauser <kirk@strauser.com>
2013-07-17 23:06:16 -0700

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Judgish',
+    version='0.1',
+    description='Analyze and judge the people you follow on Twitter',
+    license='MIT',
+    author='Kirk Strauser',
+    author_email='kirk@strauser.com',
+    url='https://github.com/kstrauser/judgish',
+    package_dir={'': 'src'},
+    py_modules=['judgish'],
+    install_requires=[
+        'twitter',
+    ]
+)
